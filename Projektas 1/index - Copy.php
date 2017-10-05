afd80e3885f54cfc5a8ba00ca50a33bdfee457b1
voldemarasp <voldemarasp@gmail.com>
2017-10-05 11:01:01 +0300

    test
    test

---
+++ Projektas 1/index - Copy.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="http://localhost/Projektas%201/FA/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>LIETUVA BUK LAISVA</title>
+</head>
+<body>
+
+	<div class="topbar">
+		<div class="topbarleft">
+			LIETUVA
+		</div>
+		<div class="topbarright">
+			<span class="topbarsocial">1</span><span class="topbarsocial">2</span><span class="topbarsocial">3</span>
+		</div>
+	</div>
+
+	<div class="header">
+		<div class="lietuva">
+			<h1>LIETUVA</h1>
+		</div>
+
+		<div class="veleva">
+			<div class="veleva1">
+
+			</div>
+			<div class="veleva2">
+
+			</div>
+			<div class="veleva3">
+
+			</div>
+		</div>
+
+	</div>
+
+	<div class="wrap">
+		<div class="content">
+			<div class="titleh2">
+				<h2>Istorija</h2>
+			</div>
+			<div class="text1">
+				<img src="images/1.jpeg" width="150">
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porta vestibulum massa non porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque porttitor in purus et tincidunt. Praesent a tellus eros. Nulla facilisis erat arcu, quis viverra metus suscipit at. Maecenas viverra pharetra metus, sit amet suscipit neque aliquam nec. In id mattis est. Sed a mauris odio.
+
+Quisque ut orci consequat, luctus turpis at, accumsan augue. Proin pharetra tincidunt nisl. Cras ac arcu in massa tincidunt maximus sit amet vel nisl. Ut vel sollicitudin ante. Ut mauris velit, ultrices ac sapien vitae, iaculis pharetra nulla. Pellentesque auctor placerat interdum. Praesent volutpat quam sit amet diam mattis, et posuere libero lobortis. Pellentesque sed porta felis. Maecenas consectetur turpis eget diam hendrerit, id eleifend dui sodales. Quisque mattis eros ac vulputate porta. Donec ultrices mollis orci, eu suscipit sapien auctor at.
+
+ Nam ut arcu at sapien fermentum eleifend euismod in ipsum. Integer ut auctor tellus, vel cursus leo. Cras nisl dui, vestibulum nec purus vitae, congue porta quam. Fusce maximus purus magna, nec tincidunt ipsum porttitor in. Donec in bibendum nulla. In ut volutpat sapien, id interdum leo. In efficitur felis sit amet lorem dignissim aliquet. Nunc sem lacus, porttitor at gravida sit amet, fringilla ac libero. Vestibulum in tellus vel ipsum hendrerit fermentum. Proin interdum congue aliquet. Phasellus placerat nibh ut est feugiat, a venenatis leo aliquam. Nullam libero nisl, finibus eu enim id, facilisis commodo dui. Nam rutrum lectus non nunc imperdiet efficitur. Aliquam auctor vitae neque et efficitur. Donec id ex libero.
+
+Cras eu gravida metus. Aenean varius cursus purus tincidunt lobortis. Nulla purus lorem, consectetur sit amet mollis a, eleifend nec erat. In tincidunt euismod dignissim. Aenean malesuada malesuada est, eget consequat massa tristique sed. Morbi aliquet mattis neque, non tincidunt nulla lacinia et. Aenean nec nisi vitae velit dictum ullamcorper at in mauris. Praesent faucibus felis et ante vulputate sollicitudin.
+
+Curabitur sodales consequat augue tincidunt rutrum. Nullam et aliquam urna. In rutrum pretium ipsum, id mollis diam iaculis id. Donec ultricies augue est, id venenatis eros venenatis vel. Suspendisse et suscipit mi. Maecenas vel est vel mauris accumsan congue. Morbi vel convallis orci, et euismod risus. Suspendisse pulvinar porttitor interdum. Nam feugiat turpis hendrerit, consectetur ligula in, tincidunt magna. Sed vehicula nisl id dictum ultricies. Fusce rhoncus ligula at massa rutrum vehicula. 
+ </p>
+			</div>
+		<div style="clear: both;"></div>
+		<div class=content2>
+			<div class="miestai">
+				<h3><i class="fa fa-home" aria-hidden="true"></i>  Miestai</h3>
+				<ol>
+					<li>Vilnius</li>
+					<li>Kaunas</li>
+					<li>Klaipeda</li>
+					<li>Vilnius</li>
+					<li>Vilnius</li>
+					<li>Vilnius</li>
+					<li>Vilnius</li>
+				</ol>
+			</div>
+			<div class="statistika">
+				<div class="statistikatitle">
+					<h3><i class="fa fa-bar-chart" aria-hidden="true"></i>  Statistika</h3>
+				</div>
+				<div class="statistika1col">
+					<ul>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+					</ul>
+				</div>
+				<div class="statistika1co1">
+					<ul>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+						<li>lorem ipsum</li>
+					</ul>
+				</div>
+			</div>	
+
+		</div>
+		<div style="clear: both;"></div>
+		<div class="imagewrap">
+		<div class="footerimage">
+			<div class="imagefooter1">
+			<img src="images/2.jpg" width="100%">
+			</div>
+			<div class="imagefooter2">
+			<img src="images/2.jpg" width="100%">
+			</div>
+		</div>
+		</div>
+		<div style="clear: both;"></div>
+		<div class="logos">
+			<div class="onelogos">
+				<img src="images/2.jpg">
+				<img src="images/2.jpg" width="14%">
+				<img src="images/2.jpg" width="14%">
+				<img src="images/2.jpg" width="14%">
+				<img src="images/2.jpg" width="14%">
+				<img src="images/2.jpg" width="14%">
+				<img src="images/2.jpg" width="14%">
+			</div>
+<!-- 			<div class="onelogos">
+				<img src="images/2.jpg" width="100%">
+			</div>			
+			<div class="onelogos">
+				<img src="images/2.jpg" width="100%">
+			</div>
+			<div class="onelogos">
+				<img src="images/2.jpg" width="100%">
+			</div>
+			<div class="onelogos">
+				<img src="images/2.jpg" width="100%">
+			</div>
+			<div class="onelogos">
+				<img src="images/2.jpg" width="100%">
+			</div>
+			<div class="onelogos">
+				<img src="images/2.jpg" width="100%">
+			</div> -->
+		</div>
+		</div>
+
+	</div>
+
+<div class="footer">
+2017
+</div>
+
+</body>
+</html>
